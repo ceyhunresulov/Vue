0f5ee4bc5c3cd3a2d146778fdb6de6b5b7e88178
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-06-08 18:16:37 +0400

    vue-cli component

---
+++ vue_task_1/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>salam</h1>
+  <p>hdksadk</p>
+  <x></x>
+</template>
+
+<script>
+import secondApp from "@/components/secondApp.vue";
+export default {
+  components: {
+    x: secondApp,
+  },
+};
+</script>
---
+++ vue_task_1/src/components/secondApp.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ counterItem }}</h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counterItem: "component",
+    };
+  },
+};
+</script>
